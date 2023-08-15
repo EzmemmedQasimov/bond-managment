1722a9fa334564c54822be9dd651971ea217cf32
azmammad.q <azmammad.q@kripton.az>
2023-08-15 19:52:26 +0400

    seeder data added for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,23 @@ class DatabaseSeeder extends Seeder
             'interest_calculation_period'       => 360,
             'coupon_interest'                   => 10
         ]);
+
+        DB::table('bond')->insert([
+            'issue_date'                        => '2021-11-08',
+            'last_circulation_date'             => '2022-11-07',
+            'nominal_price'                     => 200,
+            'coupon_payment_frequency'          => 4,
+            'interest_calculation_period'       => 364,
+            'coupon_interest'                   => 20
+        ]);
+
+        DB::table('bond')->insert([
+            'issue_date'                        => '2021-11-08',
+            'last_circulation_date'             => '2022-11-08',
+            'nominal_price'                     => 300,
+            'coupon_payment_frequency'          => 4,
+            'interest_calculation_period'       => 365,
+            'coupon_interest'                   => 30
+        ]);
     }
 }
